93fd66e6a17a811256370dedd1a31614e9067fcd
Imir25 <anitra26121998@gmail.com>
2023-11-10 18:51:35 +0300

    widget review

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,11 @@
+import React from 'react';
+import FeedbackWidget from './FeedbackWidget';
+import './styles.css'; 
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div className="app-container"> 
+      <FeedbackWidget />
     </div>
   );
 };
